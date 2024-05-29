a5a3f2b90865da1c56524eec3af66fb56a7d13a5
Eduardo-Matsunaga <eduardoborgesmatsunaga@gmail.com>
2024-05-28 22:14:13 -0300

    feat: Adjusts for links

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\EventIndexController;
 use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\GalleryIndexController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StoreCommentController;
 use App\Http\Controllers\WelcomeController;
@@ -16,10 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', WelcomeController::class)->name('welcome');
 Route::get('/e/{id}',EventShowController::class)->name('eventShow');
+Route::get('/e',EventIndexController::class)->name('eventIndex');
+Route::get('/gallery',GalleryIndexController::class)->name('galleryIndex');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {return view('dashboard');})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -29,21 +31,13 @@ Route::middleware('auth')->group(function () {
     Route::resource('/events', EventController::class);
     Route::resource('/galleries', GalleryController::class);
 
-    Route::post(
-        '/events-like/{id}',
-        LikeSystemController::class
-    )->name('events.like');
-    Route::post(
-        '/events-saved/{id}',
-        SavedEventSystemController::class
-    )->name('events.saved');
+    Route::post('/events-like/{id}', LikeSystemController::class)->name('events.like');
+    Route::post('/events-saved/{id}', SavedEventSystemController::class)->name('events.saved');
     Route::post('/events-attending/{id}', AttendingSystemController::class)->name('events.attending');
 
     Route::post('/events/{id}/comments',StoreCommentController::class)->name('events.comments');
     Route::delete('/events/{id}/comments/{comment}', DeleteCommentController::class)->name('events.comments.destroy');
-    Route::get('/countries/{country}', function (Country $country) {
-       return response()->json($country->cities);
-    });
+    Route::get('/countries/{country}', function (Country $country) {return response()->json($country->cities);});
 });
 
 require __DIR__.'/auth.php';
